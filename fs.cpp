307bb94b7bfab0fa36bb4497a77dd82a4c7c0a6b
Evgeny Novozhilov <zhenya.novozhilov@mail.ru>
2026-06-22 15:15:10 +0300

    initial commit

--- fs.cpp
+++ fs.cpp
@@ -1,9 +1,14 @@
 #include <Windows.h>
+#include <iostream>
 #include <filesystem>
 
 using dir_it = std::filesystem::directory_iterator;
 
-void print_console(const std::wstring_view& str) {
+static void usage(const wchar_t* name) {
+    std::wcout << "Usage: ./" << name << " [directory]\n"; 
+}
+
+void print_console(const std::wstring_view str) {
     WriteConsoleW(
             GetStdHandle(STD_OUTPUT_HANDLE), 
             str.data(), 
@@ -14,8 +19,17 @@ void print_console(const std::wstring_view& str) {
         putwchar('\n');
 }
 
-int wmain() {
-    std::filesystem::path path {L"C:\\Users\\user1\\Documents\\Warcraft III"};
+int wmain(int argc, wchar_t** argv) {
+    if (argc < 2) {
+        usage(argv[0]);
+        return 1;
+    }
+    std::wstring dir_name = argv[1];
+    std::filesystem::path path {dir_name};
+    if (!std::filesystem::exists(path)) {
+        std::wcerr << dir_name << " doesn't exist\n";
+        return 1;
+    }
     for (const auto& dir_entry: dir_it{path}) {
         if (dir_entry.path() == path) continue;
         print_console(dir_entry.path().native());
